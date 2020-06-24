ab7ff5224db2bd7942855a26df091770d01eefe5
upodroid <cy@borg.dev>
2020-06-24 14:10:56 +0100

    initial commit

---
+++ terraform/provider.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project = "borg-dummy-project"
+  region  = "europe-west6"
+  version = "3.24"
+}
+
+provider "google-beta" {
+  project = "borg-dummy-project"
+  region  = "europe-west6"
+  version = "3.24"
+}
+
+data "google_client_config" "provider" {}
+
+
+terraform {
+  required_version = "0.12.24"
+  backend "gcs" {
+    bucket = "mahamed-coen-state"
+    prefix = "atlantis"
+  }
+}
